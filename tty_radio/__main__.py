cff83b4b3139c29dbd9745148d2b83220bc71de2
rfarley <rfarley@mitre.org>
2016-12-21 18:28:41 -0500

    Rename term_hw, move more UI from main to radio

--- tty_radio/__main__.py
+++ tty_radio/__main__.py
@@ -1,33 +1,19 @@
 #!/usr/bin/env python
 from __future__ import print_function
 import sys
-import platform
-PY3 = False
-if platform.python_version().startswith('3'):
-    PY3 = True
 import getopt
 from os.path import expanduser
-from io import StringIO
-
-if PY3:
-    get_input = input
-else:
-    get_input = raw_input
 
 from . import (
     BANNER,
-    FAVS_CHAN_FILENAME,
-    SOMA_CHAN_FILENAME,
+    FAVS_CHAN_FILENAME,  # TODO remove import
+    SOMA_CHAN_FILENAME,  # TODO remove import
 )
 from .radio import (
-    resetDimensions,
-    printStations,
-    playStation,
-    deletePromptChars)
+    do_ui,
+    term_hw)
 from .color import colors
 from .banner import bannerize
-from .album import gen_art
-from .stationfile import get_stations
 
 
 USAGE = """Usage %s [-h|--help|-s|--soma]
@@ -44,7 +30,7 @@ And let's not forget the pretty ASCII art...
 
 def usage():
     print(USAGE % sys.argv[0])
-    (term_w, term_h) = resetDimensions()
+    (term_w, term_h) = term_hw()
     (banner, font) = bannerize("ASCII Art, FTW!", term_w)
     with colors("purple"):  # or blue, green, yellow, red
         print(banner)
@@ -71,158 +57,9 @@ def main():
             usage()
         else:
             usage()
-
     # we want the home directory to find/store the channels file
     home = expanduser("~")
-
-    # ######
-    # get the station list
-    chans = get_stations(home + "/" + chan_filename, mode)
-
-    # ######
-    # main loop
-    # shows possible stations, takes in user input, and calls player
-    # when the player is exited, this loop happens again
-    switch_mode = False
-    while(1):
-        (term_w, term_h) = resetDimensions()
-        if switch_mode != '':
-            if switch_mode == 'f':
-                mode = "favs"
-                chan_filename = FAVS_CHAN_FILENAME
-            elif switch_mode == 's':
-                mode = "soma"
-                chan_filename = SOMA_CHAN_FILENAME
-            chans = get_stations(home + "/" + chan_filename, mode)
-            switch_mode = False
-
-        # ######
-        # print stations
-        title = "unknown"
-        if mode == "favs":
-            title = "Radio Tuner"
-        elif mode == "soma":
-            title = "SomaFM Tuner"
-        with colors("red"):
-            (banner, font) = bannerize(title, term_w)
-            b_IO = StringIO(banner)
-            b_h = len(b_IO.readlines())
-            print(banner)  # , end='')
-            b_h += 1
-        (keys, line_cnt) = printStations(chans, term_w, mode)
-        loop_line_cnt = line_cnt + b_h + 2
-        loop_line_cnt += 1
-        if term_h > loop_line_cnt:
-            print('\n' * (term_h - loop_line_cnt - 1))
-
-        # ######
-        # get user input
-        switch_mode = ''
-        chan_num = None
-        while chan_num not in keys:
-            try:
-                chan_num = get_input("\nPlease select a channel [q to quit]: ")
-            except SyntaxError:
-                chan_num = ''
-            except KeyboardInterrupt:
-                return 0
-            chan_num = str(chan_num)
-            if len(chan_num) > 0:
-                chan_num = chan_num.lower()
-                ctrl_char = chan_num[0]
-                if(ctrl_char == 'q' or ctrl_char == 'e'):
-                    sys.stdout.write("\x1b]0;" + "\x07")
-                    return 0
-                # they type in some variant of either somafm or favs
-                if ctrl_char == mode[0]:
-                    break
-                elif mode == "favs" and ctrl_char == 's':
-                    switch_mode = ctrl_char
-                    break
-                elif mode == "soma" and ctrl_char == 'f':
-                    switch_mode = ctrl_char
-                    break
-            try:
-                chan_num = int(chan_num)
-                if mode == "favs":
-                    if chan_num == len(keys):
-                        switch_mode = 's'
-                        break
-                if mode == "soma":
-                    if chan_num == len(keys):
-                        switch_mode = 'f'
-                        break
-            except:
-                pass
-
-        if switch_mode != '' or (mode == "favs" and ctrl_char == 'f') or (mode == "soma" and ctrl_char == 's'):
-            continue
-
-        (term_w, term_h) = resetDimensions()
-
-        # ######
-        # call player
-        if chans[chan_num][3] != "":
-            print("ASCII Printout of Station's Logo:")
-            art = gen_art(chans[chan_num][3], term_w, term_h)
-            if art is not None:
-                print(art)
-        unhappy = True
-        while unhappy:
-            (term_w, term_h) = resetDimensions()
-            font = "unknown"
-            with colors("yellow"):
-                (banner, font) = bannerize(chans[chan_num][1], term_w)
-                b_IO = StringIO(banner)
-                b_height = len(b_IO.readlines())
-                if term_h > (b_height + 3):  # Playing, Station Name, Song Title
-                    print('\n' * (term_h - b_height - 2))
-                print(banner, end='')
-            with colors("purple"):
-                prompt = "Press enter if you like banner (font: " + font + "), else any char then enter "
-                try:
-                    happiness = get_input(prompt)
-                except SyntaxError:
-                    happiness = ''
-                deletePromptChars(len(prompt) + len(happiness))
-                if len(happiness) == 0:
-                    unhappy = False
-                    msg1 = "Playing station, enjoy..."
-                    msg2 = "[pause/quit=q; vol=+/-]"
-                    if term_w > (len(msg1) + len(msg2)):
-                        print(msg1 + ' ' + msg2)
-                    else:
-                        print(msg1)
-                        print(msg2)
-                else:
-                    print("")  # empty line for pretty factor
-        replay = True
-        show_station_deets = True
-        while replay:
-            with colors("blue"):
-                prefix = ">>> "
-                delete_cnt = playStation(chans[chan_num][0], prefix, show_station_deets)
-                deletePromptChars(delete_cnt)
-            with colors("purple"):
-                # it will reach here anytime the player stops executing (eg it has an exp, failure, etc)
-                # but ideally it'll only reach here when the user presses q (exiting the player) to "pause" it
-                # you can't use mpg123's 'pause' cmd (spacebar) bc it'll fail a minute or two after resuming (buffer errors)
-                # for some reason it literally pauses the music, buffering the stream until unpaused
-                # behavior we want is to stop recving the stream (like turing off a radio)
-                prompt = "Paused. Press enter to Resume; q to quit. "
-                reloop = get_input(prompt)
-                # if the user inputs anything other than pressing return/enter, then loop will quit
-                if len(reloop) != 0:
-                    replay = False
-                else:
-                    # for prettiness we don't want to reprint the station details (name, etc) upon replaying within this loop
-                    show_station_deets = False
-                    # we also want to get rid of that prompt to make room for the song name data again
-                    deletePromptChars(len(prompt) + len(prefix))
-                    sys.stdout.flush()
-                    # this play->pause->loop should never accumulate lines in the output (except for the first Enter they press at a prompt and even then it's just an empty line)
-            # end with
-        # end while { show list, play selected station, wait for player to exit }
+    do_ui(mode, home, chan_filename)
     return 0
 
 
--- tty_radio/radio.py
+++ tty_radio/radio.py
@@ -20,19 +20,33 @@ from __future__ import print_function
 #      perhaps get direct links for Soma
 #   ability to CRUD a favorites playlist
 #
+import platform
+PY3 = False
+if platform.python_version().startswith('3'):
+    PY3 = True
 import sys
 import textwrap
 import os
 import subprocess
 import re
 import math
+from io import StringIO
+if PY3:
+    get_input = input
+else:
+    get_input = raw_input
+
 
 from . import (
     VOL,
-    COMPACT_TITLES
+    COMPACT_TITLES,
+    FAVS_CHAN_FILENAME,
+    SOMA_CHAN_FILENAME,
 )
 from .color import colors
-# from .stationfile import
+from .banner import bannerize
+from .album import gen_art
+from .stationfile import get_stations
 
 
 def printStations(chans, term_w, mode):
@@ -112,7 +126,7 @@ def playStation(url, prefix, show_deets=1):
             station_deets = out[10:]
             if station_deets[0:3] != "Def":  # bc it's kinda poser having so much 'defcon' all over your screen
                 # print(">>> " +  station_deets)
-                (term_w, term_h) = resetDimensions()
+                (term_w, term_h) = term_hw()
                 lines = textwrap.wrap(station_deets, term_w - len(prefix))
                 print(prefix + lines[0])
                 for line in lines[1:]:
@@ -127,7 +141,7 @@ def playStation(url, prefix, show_deets=1):
             except:
                 song_title = "Song title didn't parse(" + out + ")"
             # confine song title to single line to look good
-            (term_w, term_h) = resetDimensions()
+            (term_w, term_h) = term_hw()
             if len(song_title) > (term_w - len(prefix)):
                 song_title = song_title[0:(term_w - len(prefix))]
             # this will delete the last song, and reuse its line (so output only has one song title line)
@@ -157,13 +171,13 @@ def deleteChars(num_chars):
 def deletePromptChars(num_chars):
     # determine lines to move up, there is at least 1 bc user pressed enter to give input
     # when they pressed Enter, the cursor went to beginning of the line
-    (term_w, term_h) = resetDimensions()
+    (term_w, term_h) = term_hw()
     move_up = int(math.ceil(float(num_chars) / float(term_w)))
     print("\033[A" * move_up + ' ' * num_chars + '\b' * (num_chars), end='')
     return
 
 
-def resetDimensions():
+def term_hw():
     try:
         # *nix get terminal/console width
         rows, columns = os.popen('stty size', 'r').read().split()
@@ -173,3 +187,153 @@ def resetDimensions():
         # print("term width: %d"% width)
     except:
         return (80, 40)
+
+
+def do_ui(mode, home, chan_filename):
+    # ######
+    # get the station list
+    chans = get_stations(home + "/" + chan_filename, mode)
+    # ######
+    # main loop
+    # shows possible stations, takes in user input, and calls player
+    # when the player is exited, this loop happens again
+    switch_mode = False
+    while(1):
+        (term_w, term_h) = term_hw()
+        if switch_mode != '':
+            if switch_mode == 'f':
+                mode = "favs"
+                chan_filename = FAVS_CHAN_FILENAME
+            elif switch_mode == 's':
+                mode = "soma"
+                chan_filename = SOMA_CHAN_FILENAME
+            chans = get_stations(home + "/" + chan_filename, mode)
+            switch_mode = False
+
+        # ######
+        # print stations
+        title = "unknown"
+        if mode == "favs":
+            title = "Radio Tuner"
+        elif mode == "soma":
+            title = "SomaFM Tuner"
+        with colors("red"):
+            (banner, font) = bannerize(title, term_w)
+            b_IO = StringIO(banner)
+            b_h = len(b_IO.readlines())
+            print(banner)  # , end='')
+            b_h += 1
+        (keys, line_cnt) = printStations(chans, term_w, mode)
+        loop_line_cnt = line_cnt + b_h + 2
+        loop_line_cnt += 1
+        if term_h > loop_line_cnt:
+            print('\n' * (term_h - loop_line_cnt - 1))
+
+        # ######
+        # get user input
+        switch_mode = ''
+        chan_num = None
+        while chan_num not in keys:
+            try:
+                chan_num = get_input("\nPlease select a channel [q to quit]: ")
+            except SyntaxError:
+                chan_num = ''
+            except KeyboardInterrupt:
+                return 0
+            chan_num = str(chan_num)
+            if len(chan_num) > 0:
+                chan_num = chan_num.lower()
+                ctrl_char = chan_num[0]
+                if(ctrl_char == 'q' or ctrl_char == 'e'):
+                    sys.stdout.write("\x1b]0;" + "\x07")
+                    return 0
+                # they type in some variant of either somafm or favs
+                if ctrl_char == mode[0]:
+                    break
+                elif mode == "favs" and ctrl_char == 's':
+                    switch_mode = ctrl_char
+                    break
+                elif mode == "soma" and ctrl_char == 'f':
+                    switch_mode = ctrl_char
+                    break
+            try:
+                chan_num = int(chan_num)
+                if mode == "favs":
+                    if chan_num == len(keys):
+                        switch_mode = 's'
+                        break
+                if mode == "soma":
+                    if chan_num == len(keys):
+                        switch_mode = 'f'
+                        break
+            except:
+                pass
+
+        if switch_mode != '' or (mode == "favs" and ctrl_char == 'f') or (mode == "soma" and ctrl_char == 's'):
+            continue
+
+        (term_w, term_h) = term_hw()
+
+        # ######
+        # call player
+        if chans[chan_num][3] != "":
+            print("ASCII Printout of Station's Logo:")
+            art = gen_art(chans[chan_num][3], term_w, term_h)
+            if art is not None:
+                print(art)
+        unhappy = True
+        while unhappy:
+            (term_w, term_h) = term_hw()
+            font = "unknown"
+            with colors("yellow"):
+                (banner, font) = bannerize(chans[chan_num][1], term_w)
+                b_IO = StringIO(banner)
+                b_height = len(b_IO.readlines())
+                if term_h > (b_height + 3):  # Playing, Station Name, Song Title
+                    print('\n' * (term_h - b_height - 2))
+                print(banner, end='')
+            with colors("purple"):
+                prompt = "Press enter if you like banner (font: " + font + "), else any char then enter "
+                try:
+                    happiness = get_input(prompt)
+                except SyntaxError:
+                    happiness = ''
+                deletePromptChars(len(prompt) + len(happiness))
+                if len(happiness) == 0:
+                    unhappy = False
+                    msg1 = "Playing station, enjoy..."
+                    msg2 = "[pause/quit=q; vol=+/-]"
+                    if term_w > (len(msg1) + len(msg2)):
+                        print(msg1 + ' ' + msg2)
+                    else:
+                        print(msg1)
+                        print(msg2)
+                else:
+                    print("")  # empty line for pretty factor
+        replay = True
+        show_station_deets = True
+        while replay:
+            with colors("blue"):
+                prefix = ">>> "
+                delete_cnt = playStation(chans[chan_num][0], prefix, show_station_deets)
+                deletePromptChars(delete_cnt)
+            with colors("purple"):
+                # it will reach here anytime the player stops executing (eg it has an exp, failure, etc)
+                # but ideally it'll only reach here when the user presses q (exiting the player) to "pause" it
+                # you can't use mpg123's 'pause' cmd (spacebar) bc it'll fail a minute or two after resuming (buffer errors)
+                # for some reason it literally pauses the music, buffering the stream until unpaused
+                # behavior we want is to stop recving the stream (like turing off a radio)
+                prompt = "Paused. Press enter to Resume; q to quit. "
+                reloop = get_input(prompt)
+                # if the user inputs anything other than pressing return/enter, then loop will quit
+                if len(reloop) != 0:
+                    replay = False
+                else:
+                    # for prettiness we don't want to reprint the station details (name, etc) upon replaying within this loop
+                    show_station_deets = False
+                    # we also want to get rid of that prompt to make room for the song name data again
+                    deletePromptChars(len(prompt) + len(prefix))
+                    sys.stdout.flush()
+                    # this play->pause->loop should never accumulate lines in the output (except for the first Enter they press at a prompt and even then it's just an empty line)
+            # end with
+        # end while { show list, play selected station, wait for player to exit }
