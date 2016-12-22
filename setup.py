bc226372cb38068685687f96a66126689d1a41e4
rfarley <rfarley@mitre.org>
2016-12-22 02:31:14 -0500

    Prep for pypi

--- setup.py
+++ setup.py
@@ -2,11 +2,15 @@ from setuptools import setup
 
 setup(
     name='tty_radio',
+    packages=['tty_radio'],
     version='0.1.4',
     description="Terminal player for online radio streams.",
-    author='rfarley3',
+    author='Ryan Farley',
     author_email='rfarley3@gmu.edu',
-    packages=['tty_radio'],
+    url='https://github.com/rfarley3/radio',
+    download_url='https://github.com/rfarley3/radio/tarball/0.1.4',
+    keywords=['radio', 'somafm', 'streaming', 'mpg123'],
+    classifiers=[],
     install_requires=[
         'beautifulsoup4',
         'pyfiglet',
