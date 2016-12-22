5fe160b93c028f6d924052574f7d88bbcf37bb99
rfarley <rfarley@mitre.org>
2016-12-22 02:40:36 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name='tty_radio',
     packages=['tty_radio'],
-    version='0.1.4',
+    version='0.1.5',
     description="Terminal player for online radio streams.",
     author='Ryan Farley',
     author_email='rfarley3@gmu.edu',
     url='https://github.com/rfarley3/radio',
-    download_url='https://github.com/rfarley3/radio/tarball/0.1.4',
+    download_url='https://github.com/rfarley3/radio/tarball/0.1.5',
     keywords=['radio', 'somafm', 'streaming', 'mpg123'],
     classifiers=[],
     install_requires=[
