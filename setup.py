b622d00353944501f17d2760ca9ce51d937774ae
rfarley <rfarley@mitre.org>
2016-12-23 17:24:13 -0500

    Bump version to 1.0.0

--- setup.py
+++ setup.py
@@ -3,14 +3,15 @@ from setuptools import setup
 setup(
     name='tty_radio',
     packages=['tty_radio'],
-    version='0.1.5',
+    version='1.0.0',
     description=(
-        "Linux/OS X terminal player for online radio streams, " +
-        "like SomaFM and WCPE. Flavored with colors and ASCII art."),
+        "Linux/OS X RESTful player for online radio streams, " +
+        "like SomaFM and WCPE. Comes with a terminal UI " +
+        "flavored with colors and ASCII art."),
     author='Ryan Farley',
     author_email='rfarley3@gmu.edu',
     url='https://github.com/rfarley3/radio',
-    download_url='https://github.com/rfarley3/radio/tarball/0.1.5',
+    download_url='https://github.com/rfarley3/radio/tarball/1.0.0',
     keywords=['radio', 'somafm', 'streaming', 'mpg123'],
     classifiers=[],
     install_requires=[
